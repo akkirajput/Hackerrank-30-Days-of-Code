e8dc9da1c682d49c8968e4be77a1ebb2a8a6673f
Ankit Singh Chauhan <ankitchouhan819@gmail.com>
2020-10-07 16:45:57 +0530

    Create Day 27: Testing.cpp

---
+++ Day 27: Testing.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <cassert>
+#include <set>
+
+using namespace std;
+
+int minimum_index(vector<int> seq) {
+    if (seq.empty()) {
+        throw invalid_argument("Cannot get the minimum value index from an empty sequence");
+    }
+    int min_idx = 0;
+    for (int i = 1; i < seq.size(); ++i) {
+        if (seq[i] < seq[min_idx]) {
+            min_idx = i;
+        }
+    }
+    return min_idx;
+}
+
+class TestDataEmptyArray {
+public:
+    static vector<int> get_array() {
+        vector<int> v{};
+        return v;
+    }
+
+};
+
+class TestDataUniqueValues {
+public:
+    static vector<int> get_array() {
+        vector<int> v{1,2,0,4,5};
+        return v;
+    }
+
+    static int get_expected_result(){
+        return 2;
+    }
+
+};
+
+class TestDataExactlyTwoDifferentMinimums {
+public:
+    static vector<int> get_array() {
+       vector<int> v{2,3,1,4,5,1,7};
+        return v;
+    }
+
+    static int get_expected_result() {
+        return 2;
+    }
+
+};
+
